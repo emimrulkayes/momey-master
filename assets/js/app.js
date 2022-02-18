224c831a8ce2b38600d0bd0bf78615e22056ba3c
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-18 11:38:17 +0600

    added responsive

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+function calculate() {
+    const foodCost = document.getElementById('food-cost');
+    const rentCost = document.getElementById('rent-cost');
+    const clothCost = document.getElementById('cloth-cost');
+
+}
